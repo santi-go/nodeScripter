d961bb5211fc9b0527b43939c38e4e4a57172378
Santiago Gonzalez Erazo <santiago.gonzalez.erazo@gmail.com>
2020-03-02 18:52:29 +0100

    first raw data attemp - broken

--- multiChoiceAnswers.js
+++ multiChoiceAnswers.js
@@ -1,4 +1,4 @@
-let dt = require('./values')
+let dt = require('./proccesed/values')
 
 exports.multiChoiceAnswers = {
     values: dt.values,
@@ -11,16 +11,16 @@ exports.multiChoiceAnswers = {
         "(@SiteID, @Lang, @Device, 'group_100', 244, 'Old Schoolhouse (Glasgow)', 'Old Schoolhouse (Glasgow)', 'Old Schoolhouse (Glasgow)', NULL, 'belhaven', NULL),"
     ],
     sequence : "parts[0]+sortNumber+parts[1]+el+parts[1]+el+parts[1]+el+parts[2]",
+    indexOffset: 0,
     assambler: (e)  => {
         let lines = [];
 
         e.values.forEach((el, index) => {
             let sortNumber = (index+1) + e.indexOffset;
-
+            
             lines.push(e.parts[0]+sortNumber+e.parts[1]+el+e.parts[1]+el+e.parts[1]+el+e.parts[2]);
         });
         
         return lines;
-    },
-    indexOffset: 0
+    }  
 }
--- nodeScripter.js
+++ nodeScripter.js
@@ -1,9 +1,11 @@
 let fs = require('fs');
 let mca = require('./multiChoiceAnswers')
 let iu = require('./insertUsers')
+let vs = require('./createValues')
 
 function printer(e) {
-    let data = packer(e);
+    packer()
+    let data = assembler(e);
     
     fs.writeFile('proccesed/script.sql', data, (err) => {
         if(err) return console.log(err);
@@ -11,12 +13,32 @@ function printer(e) {
     });
 }
 
-function packer(e) {
-    let package = e.assambler(e);
+function assembler(e) {
+    let assembled = e.assambler(e);
     
-    return package.join("");
+    return assembled.join("");
 }
 
-//printer(mca.multiChoiceAnswers);
+function packer() {
+    let values = tranmute();
+    createValues(values, vs.createValues);
+}
 
-printer(iu.insertUsers);
+function tranmute() {
+    let stone = fs.readFileSync('./raw.txt', 'utf-8').split("\n");
+
+    return stone;
+}
+
+function createValues (v, e) {
+    return vs.createValues.assambler(v, e);
+}
+
+printer(mca.multiChoiceAnswers);
+
+//printer(iu.insertUsers);
+
+//packer()
+
+
+/*hacer configuraci'on de creacion de script values.js*/
